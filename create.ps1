7efc8140148e2b1ccc3dffa373c0948ec1c4ef2c
Peter Hoch <peter-hoch@users.noreply.github.com>
2025-04-11 18:15:40 +0200

    test assembly added

---
+++ create.ps1
@@ -0,0 +1,3 @@
+.\DotNetToJScript\bin\x64\Release\DotNetToJScript.exe .\ExampleAssembly\bin\x64\Release\ExampleAssembly.dll --lang=Jscript --ver=v4 -o ExampleAssembly.js
+
+.\DotNetToJScript\bin\x64\Release\DotNetToJScript.exe .\StartCalculator\bin\x64\Release\StartCalculator.dll  --lang=Jscript --ver=v4 -o StartCalculator.js -c StartCalculator.TestClass
